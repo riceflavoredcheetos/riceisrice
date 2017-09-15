53633e0d5119f47f0e85783feb4d08ea1a169e97
jordanjpark <jordanjpark@gmail.com>
2017-09-15 02:51:39 -0400

    changed camelCase

--- client/components/SingleProductPage.js
+++ client/components/SingleProductPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
 import {getSingleProduct} from '../store/singleProduct'
-import Review from './Reviews'
+import Review from './reviews'
 
 
 class SingleProduct extends React.Component{
