719cf4900c071d7b4734b2cc44bcc50da7ac720a
jordanjpark <jordanjpark@gmail.com>
2017-09-15 11:03:58 -0400

    changed all components to UpperUpper.js

---
+++ client/components/AllProducts.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter, Link } from "react-router-dom";
+import { logout } from "../store";
+import { getAllProducts } from "../store/allProducts";
+
+class Product extends React.Component {
+  componentDidMount() {
+    this.props.getProducts();
+  }
+
+  render() {
+    const rice = this.props.AllProducts;
+    console.log(this.props);
+    console.log("Rice:", rice);
+    return (
+      <div>
+        <h1>ALL PRODUCTS</h1>
+        <div className="list-group">
+          {rice.map(item => {
+            return (
+              <Link
+                to={`/product/${item.id}`}
+                className="list-group-item"
+                key={item.id} >
+                <h4 className="list-group-item-heading">{item.title}</h4>
+                <p className="list-group-item-text">{item.description}</p>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapState = state => {
+  return {
+    AllProducts: state.AllProducts
+  };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    getProducts: function() {
+      const action = getAllProducts();
+      dispatch(action);
+    }
+  };``
+} ;
+
+export default withRouter(connect(mapState, mapDispatch)(Product));
--- client/components/index.js
+++ client/components/index.js
@@ -3,14 +3,14 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export { default as Main } from "./main";
-export { default as UserHome } from "./user-home";
-export { default as TopNavBar } from "./topNavBar";
-export { default as SearchBar } from "./searchBar";
-export { default as LoginPage } from "./loginPage";
-export { default as SingleProduct } from "./singleProductPage";
-export { default as Cart } from "./cartPage";
-export { default as Checkout } from "./checkoutPage";
-export { default as About } from "./about";
+export { default as Main } from "./Main";
+export { default as UserHome } from "./UserHome";
+export { default as TopNavBar } from "./TopNavBar";
+export { default as SearchBar } from "./SearchBar";
+export { default as LoginPage } from "./LoginPage";
+export { default as SingleProduct } from "./SingleProductPage";
+export { default as Cart } from "./CartPage";
+export { default as Checkout } from "./CheckoutPage";
+export { default as About } from "./About";
 
 export { Login, Signup } from "./auth-form";
