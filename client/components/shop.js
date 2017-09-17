4f4355c67327d07fa723dde43b64ce9064b7fca8
jordanjpark <jordanjpark@gmail.com>
2017-09-17 16:17:52 -0400

    searchbar now working properly in shop component

--- client/components/shop.js
+++ client/components/shop.js
@@ -6,26 +6,57 @@ import { logout } from "../store";
 import { getAllProducts } from "../store/allProducts";
 
 class Products extends React.Component {
+  constructor() {
+    super();
+    this.state = { keyword: "", searchProduct: false };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
   componentDidMount() {
     this.props.getProducts();
   }
 
   render() {
-    const rice = this.props.AllProduct;
-    console.log("SHOP")
+    let rice = this.props.AllProduct;
+    let title = 'ALL PRODUCTS';
+    const search = this.state.searchProduct;
+    const keyword = this.state.keyword;
+    if (search) {
+      rice = rice.filter(item => item.title.toLowerCase().includes(keyword.toLowerCase()) || item.description.toLowerCase().includes(keyword.toLowerCase()))
+      title = rice.length + ' products for ' + keyword;
+    }
     const Center = {
       textAlign: "center"
     };
+
     return (
       <div>
-        <h1 style = {Center}>ALL PRODUCTS</h1>
+        <form onSubmit={this.onFormSubmit} className="input-group">
+          <input
+            placeholder="name of the product"
+            className="form-control"
+            value={this.state.keyword}
+            onChange={this.onInputChange}
+          />
+          <span className="input-group-btn">
+            <button type="submit">
+              <img
+                src="http://downloadicons.net/sites/default/files/search-icon-93110.png"
+                height="41"
+              />
+            </button>
+          </span>
+        </form>
+        <h1 style={Center}>{title}</h1>
         <div className="list-group">
           {rice.map(item => {
             return (
               <Link
                 to={`/product/${item.id}`}
                 className="list-group-item"
-                key={item.id} >
+                key={item.id}
+              >
                 <h4 className="list-group-item-heading">{item.title}</h4>
                 <p className="list-group-item-text">{item.description}</p>
               </Link>
@@ -34,6 +65,16 @@ class Products extends React.Component {
         </div>
       </div>
     );
+  }
+
+  onInputChange(event) {
+    this.setState({ keyword: event.target.value, searchProduct: false });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    // this.props.fetchSearchProducts();
+    this.setState({ searchProduct: true });
   }
 }
 
@@ -49,10 +90,7 @@ const mapDispatch = dispatch => {
       const action = getAllProducts();
       dispatch(action);
     }
-  };``
-} ;
+  };
+};
 
 export default withRouter(connect(mapStates, mapDispatch)(Products));
-
-
-
